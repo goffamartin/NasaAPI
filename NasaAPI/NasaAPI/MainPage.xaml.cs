14aa9af5d2ceead98e1ed6673e857c11ca66478f
Martin Goffa <goffamartin@gmail.com>
2021-11-17 12:13:03 +0100

    Working communication with API (Conversion from Json to Object)

--- NasaAPI/NasaAPI/MainPage.xaml.cs
+++ NasaAPI/NasaAPI/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace NasaAPI
 {
@@ -19,13 +20,15 @@ namespace NasaAPI
         }
         private async void GetInfo()
         {
-            string url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-09-10end_date=2021-09-10&api_key=J4Yf2ueQxs6t8OeWtQOJzP5mH2y99gJIgGezfgVE";
+            string url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-09-10&end_date=2021-09-10&api_key=J4Yf2ueQxs6t8OeWtQOJzP5mH2y99gJIgGezfgVE";
             API_Response data = await API_Caller.Get(url);
 
             if (data.Successuful)
             {
-                var info = JsonConvert.DeserializeObject<Rootobject>(data.Response);
-                Console.WriteLine(info.links.next);
+                Regex reg = new Regex(@"\d{4}[-]\d{2}[-]\d{2}");
+                string database = reg.Replace(data.Response, "date",1,500);
+                var info = JsonConvert.DeserializeObject<Rootobject>(database);
+                Console.WriteLine(info.near_earth_objects.asteroids[0].close_approach_data[0].close_approach_date);
             }
         }
     }
